3e2218e77b5af115bda997a2d1e68d48a8452663
satang <thazxs.15@gmail.com>
2024-09-11 11:31:05 +0700

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-// ignore: depend_on_referenced_packages
-import 'package:labshared_pref/main.dart';
+import 'package:week9_652021063/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
